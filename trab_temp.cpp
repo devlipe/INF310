9849d164988d4922302875ba9b1e3bfe179d8e24
Felipe Ferreira <felipe.p.ferreira@ufv.br>
2022-11-10 20:39:44 -0300

    Trabalho 1 Feito

--- trab_temp.cpp
+++ trab_temp.cpp
@@ -1,9 +1,35 @@
+#include <map>
+#include <pthread.h>
+
+pthread_mutex_t mux = PTHREAD_MUTEX_INITIALIZER;
+std::map<pthread_t, int> thread_map;
+
+void block() {
+  bool sair = false;
+  long me = pthread_self();
+
+  do {
+    pthread_mutex_lock(&mux);
+    if (thread_map[me] > 0) {
+      thread_map[me]--;
+      sair = true;
+    }
+    pthread_mutex_unlock(&mux);
+  } while (!sair);
+}
+
+void wakeup(pthread_t thread) {
+  pthread_mutex_lock(&mux);
+
+  thread_map[thread]++;
+
+  pthread_mutex_unlock(&mux);
+}
+
 //! Definitions of necessary libraries
-#include "my_sync.h"
 #include <assert.h>
 #include <iostream>
 #include <map>
-#include <pthread.h>
 #include <queue>
 #include <string>
 //! Namespace used
